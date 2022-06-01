9ee11d49bfd4a192267dbf2e43511b9fea2c691c
QuarshieHector <haquarshie@gmail.com>
2022-06-01 16:36:26 +0000

    first commit

---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,30 @@
+import flutter from '../assets/flutter.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+
+
+
+
+
+
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+
+
+export default {
+    profile,
+    circle,
+    flutter,
+    redux,
+    sass,
+    about01,
+    about02,
+    about03,
+    email,
+    mobile
+}
